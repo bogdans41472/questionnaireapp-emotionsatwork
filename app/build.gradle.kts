4ece4646e276c90cdd5cfd058088552d44cc0f96
Mihai-Bogdan Sirbu <mihaibogdans@gmail.com>
2024-09-01 14:58:42 +0200

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.emotionsatwork.questionnaireapp"
         minSdk = 27
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.10"
+        versionCode = 11
+        versionName = "1.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
